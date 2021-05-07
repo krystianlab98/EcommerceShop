38f173ad57d0933714bf344083ba41be5c2eb111
krystianlab98 <krystianlab98@gmail.com>
2021-05-07 11:36:22 +0200

    Refactor Role entity and repository

--- EcommerceShopProject/EcommerceShopCommon/src/main/java/com/ecommerceshop/common/entity/Role.java
+++ EcommerceShopProject/EcommerceShopCommon/src/main/java/com/ecommerceshop/common/entity/Role.java
@@ -1,6 +1,7 @@
 package com.ecommerceshop.common.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
@@ -48,5 +49,26 @@ public class Role {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id.equals(role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
--- EcommerceShopProject/EcommerceShopWebParent/EcommerceShopBackEnd/src/main/java/com/ecommerceshop/admin/user/RoleRepository.java
+++ EcommerceShopProject/EcommerceShopWebParent/EcommerceShopBackEnd/src/main/java/com/ecommerceshop/admin/user/RoleRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
